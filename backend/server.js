e8157bb2ddc359e9668c5463c482dcfcf1259c17
Soham <sohamkelaskar@gmail.com>
2024-03-29 22:51:20 +0530

    mrudula is awesome

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const cors = require('cors');
 const User = require('./User'); // Import user model
+const path = require('path');
+
 
 const app = express();
 
@@ -22,29 +24,61 @@ mongoose.connection.once('open', () => {
 app.use(cors());
 app.use(express.json());
 
+// Set up static file serving
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Authentication route
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'index.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'login.html'));
+});
+
+app.get('/wishlist', (req, res) => {
+    // res.render('wishlist');
+    res.sendFile(path.join(__dirname, '..', 'wishlist.html'));
+});
+
+app.get('/readlist', (req, res) => {
+    // res.render('wishlist');
+    res.sendFile(path.join(__dirname, '..', 'readlist.html'));
+});
+
+app.get('/book', (req, res) => {
+    // res.render('wishlist');
+    res.sendFile(path.join(__dirname, '..', 'book.html'));
+});
+
 app.post('/login', async (req, res) => {
     const { email, password } = req.body;
 
+
     try {
-        // Find user by email
-        const user = await User.findOne({ email });
+        console.log('Soham is grt and entered in the login');
+        res.sendFile(path.join(__dirname, '..', 'wishlist.html'));
 
-        // If user doesn't exist, return error
-        if (!user) {
-            return res.status(404).json({ message: 'User not found' });
-        }
 
-        // Check if password matches
-        const isPasswordValid = await bcrypt.compare(password, user.password);
+        // // Find user by email
+        // const user = await User.findOne({ email });
 
-        // If password is invalid, return error
-        if (!isPasswordValid) {
-            return res.status(401).json({ message: 'Invalid password' });
-        }
+        // // If user doesn't exist, return error
+        // if (!user) {
+        //     return res.status(404).json({ message: 'User not found' });
+        // }
 
-        // Password is valid, user is authenticated
-        res.status(200).json({ message: 'Login successful', user });
+        // // Check if password matches
+        // const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        // // If password is invalid, return error
+        // if (!isPasswordValid) {
+        //     return res.status(401).json({ message: 'Invalid password' });
+        // }
+
+        // // Password is valid, user is authenticated
+        // res.status(200).json({ message: 'Login successful', user });
     } catch (error) {
         // Handle any errors
         console.error('Error during login:', error);
